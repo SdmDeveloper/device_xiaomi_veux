dea2e71db0a9a7823f27760d7e8d904dbdd8ee0e
sdm695dev <sdm695dev@gmail.com>
2026-03-18 15:24:34 +0000

    veux: Create vendorsetup.sh
    Signed-off-by: SᴅᴍDᴇᴠᴇʟᴏᴘᴇʀ <sdm695dev@gmail.com>

---
+++ vendorsetup.sh
@@ -0,0 +1,6 @@
+git clone https://github.com/sdmdeveloper/vendor_xiaomi_veux.git vendor/xiaomi/veux --depth=1
+git clone https://github.com/sdmdeveloper/kernel_xiaomi_sm6375 kernel/xiaomi/sm6375 --depth=1
+git clone https://github.com/LineageOS/android_hardware_xiaomi.git hardware/xiaomi
+git clone https://github.com/frost-testzone/vendor_oneplus_dolby vendor/oneplus/dolby --depth=1
+git clone -b pos https://github.com/frost-testzone/vendor_xiaomi_miuicamera-veux.git vendor/xiaomi/miuicamera-veux --depth=1
+git clone https://github.com/ProjectInfinity-X/vendor_infinity-priv_keys vendor/infinity-priv/keys
