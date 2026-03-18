dd50d1a389e7f1e6d9988b7fc4e86535d6b162e9
sdm695dev <sdm695dev@gmail.com>
2026-03-18 15:13:33 +0000

    veux: Welcome Infinity-X
    Signed-off-by: sdm695dev <sdm695dev@gmail.com>

--- infinity_veux.mk
+++ infinity_veux.mk
@@ -11,8 +11,15 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from device.
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
 
-# Inherit some common PixelOS stuff.
-$(call inherit-product, vendor/custom/config/common_full_phone.mk)
+# Inherit some common Infinity-X stuff.
+$(call inherit-product, vendor/infinity/config/common_full_phone.mk)
+
+# Infinity-X flags
+WITH_GAPPS := true
+TARGET_SUPPORTS_BLUR := true
+INFINITY_MAINTAINER := SdmDeveloper
+INFINITY_BUILD_TYPE := OFFICIAL
+USE_MOTO_CALCULATOR := true
 
 # Boot animation
 TARGET_SCREEN_HEIGHT := 2400
@@ -22,7 +29,7 @@ PRODUCT_BRAND := Redmi
 PRODUCT_DEVICE := veux
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_MODEL := 2201116SG
-PRODUCT_NAME := custom_veux
+PRODUCT_NAME := infinity_veux
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
